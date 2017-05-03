cb7bf4a231716855f625f9539c4dd03a6a8dd93b
Luiz Felipe <frazaolipe@gmail.com>
2017-05-03 20:04:20 -0300

    Update rhc.js

--- assets/others-js/rhc.js
+++ assets/others-js/rhc.js
@@ -4,28 +4,6 @@
  * @author Luiz~
  * @version: 1.0
  */
-// (function ($) {
-//   'use strict';
-
-//   $(function () {
-//     $('.post').each(function () {
-
-//       var $this = $(this);
-//       var $name = $('.postprofile-name a[href="/u78"]');
-
-//       $name
-//         .parents('.post')
-//           .find('.postprofile-rank')
-//             .html([
-//               '<!-- Conteúdo modificado pelo fórum automáticamente: -->',
-//               'Copiador de formulários do Luiz',
-//               '<br>',
-//               '<img src="https://illiweb.com/itest/ranks/default/default1.gif" title="Copiador de formulários do luiz. >:(">',
-//             ].join( '\n'))
-//       ;
-//     });
-//   });
-// }(jQuery));
 
 /** Hippei: */
 (function ($) {
@@ -84,6 +62,30 @@
               'background-color: rgb(0, 0, 0);',
               'border-bottom: none;',
               'background: linear-gradient(141deg, #000000 0%, rgb(136, 72, 134) 51%, #cf66cc 75%);',
+            ].join('\n'))
+      ;
+    });
+  });
+}(jQuery));
+
+
+/** Bruno (Guime): */
+(function ($) {
+  'use strict';
+
+  $(function () {
+    $('.post').each(function () {
+
+      var $this = $(this);
+      var $name = $('.postprofile-name a[href="/u44"]');
+
+      $name
+        .parents('.post')
+          .find('.post-head')
+            .attr('style', [
+              'background-color: #2bc2b5;',
+              'border-bottom: none;',
+              'background: linear-gradient(141deg, #2bc2b5 0%, rgb(90, 151, 141) 51%, #ff0000 75%);',
             ].join('\n'))
       ;
     });
