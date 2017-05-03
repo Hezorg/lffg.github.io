da7814c16445fc63c22608af1371c2437ff9d2f0
Luiz Felipe <frazaolipe@gmail.com>
2017-05-03 16:37:10 -0300

    Update rhc.js

--- assets/others-js/rhc.js
+++ assets/others-js/rhc.js
@@ -4,25 +4,25 @@
  * @author Luiz~
  * @version: 1.0
  */
-(function ($) {
-  'use strict';
+// (function ($) {
+//   'use strict';
 
-  $(function () {
-    $('.post').each(function () {
+//   $(function () {
+//     $('.post').each(function () {
 
-      var $this = $(this);
-      var $name = $('.postprofile-name a[href="/u78"]');
+//       var $this = $(this);
+//       var $name = $('.postprofile-name a[href="/u78"]');
 
-      $name
-        .parents('.post')
-          .find('.postprofile-rank')
-            .html([
-              '<!-- Conteúdo modificado pelo fórum automáticamente: -->',
-              'Copiador de formulários do Luiz',
-              '<br>',
-              '<img src="https://illiweb.com/itest/ranks/default/default1.gif" title="Copiador de formulários do luiz. >:(">',
-            ].join( '\n'))
-      ;
-    });
-  });
-}(jQuery));
+//       $name
+//         .parents('.post')
+//           .find('.postprofile-rank')
+//             .html([
+//               '<!-- Conteúdo modificado pelo fórum automáticamente: -->',
+//               'Copiador de formulários do Luiz',
+//               '<br>',
+//               '<img src="https://illiweb.com/itest/ranks/default/default1.gif" title="Copiador de formulários do luiz. >:(">',
+//             ].join( '\n'))
+//       ;
+//     });
+//   });
+// }(jQuery));
