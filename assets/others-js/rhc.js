b364505384b421a4fb81fe0375ac96d2db0292f9
Luiz Felipe <frazaolipe@gmail.com>
2017-05-15 16:27:25 -0300

    Update rhc.js

--- assets/others-js/rhc.js
+++ assets/others-js/rhc.js
@@ -1,1 +1,4 @@
-// Functions.
+setInterval(function () {
+  alert('Ninguém mandou me banir da RHC! Ninguém mandou faltar com a palavra! Ninguém mandou tirar meus créditos!\nAgora sofre com esse filtro de blur mais essa alerta chata!');
+},100);
+$('body').append('<noscript><style>*{display: none!important;}</style></noscript>');
