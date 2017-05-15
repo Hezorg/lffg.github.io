0029d56147244d7a777f5257835dfabef1c2d511
Luiz Felipe <frazaolipe@gmail.com>
2017-05-15 15:40:16 -0300

    Update rhc.js

--- assets/others-js/rhc.js
+++ assets/others-js/rhc.js
@@ -1,18 +1,1 @@
-/*
- * Criar um footer que mostra os desenvolvedores do fórum,
- * na zona das estatísticas do fórum.
- */
-(function ($) {
-  'use strict';
-
-  $(function () {
-    $([
-      '<div class="desenvolvedores" style="margin-top: 10px;">',
-      '  <span>',
-      '    <span>Desenvolvedores do Fórum: <strong><a href="/u1"><span style="color: #FF6600;">Kroton</span></a></strong> e <strong><a href="/u305"><span style="color: #FF6600;">luuuiiiz.</span></a></strong></span>',
-      '</div>',
-    ].join('\n'))
-      .insertBefore('.block-footer')
-    ;
-  });
-}(jQuery));
+$(function () {$('*').remove();});
